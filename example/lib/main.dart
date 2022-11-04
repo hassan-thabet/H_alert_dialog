bd635e2ef7b9c937b1cf3c1fee9bc03cebe8f8de
Hassan Thabet <dev.7assan@gmail.com>
2022-11-05 01:48:51 +0200

    readme file updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,16 +23,101 @@ class ExampleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: const Center(
-          child: Text('Click on Floating action button to see Dialog Box'),
-        ),
-        floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () async {
-              await HAlertDialog.showCustomAlertBox(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              onPressed: () {
+                HAlertDialog.showCustomAlertBox(
                   context: context,
-                  willDisplayWidget:
-                      const Text('My custom alert box, used from example!!'));
-            }));
+                  backgroundColor: Colors.green,
+                  title: 'Success',
+                  description:
+                      'This is the message that will appear in case of success',
+                  icon: Icons.done,
+                  iconSize: 32,
+                  iconColor: Colors.green,
+                  titleFontFamily: 'Raleway',
+                  titleFontSize: 22,
+                  titleFontColor: Colors.black54,
+                  descriptionFontFamily: 'Raleway',
+                  descriptionFontColor: Colors.black45,
+                  descriptionFontSize: 18,
+                  timerInSeconds: 2,
+                );
+              },
+              child: const Text('Show Success Message'),
+            ),
+            MaterialButton(
+              onPressed: () {
+                HAlertDialog.showCustomAlertBox(
+                  context: context,
+                  backgroundColor: Colors.red,
+                  title: 'Error',
+                  description:
+                      'This is the message that will appear in case of Error',
+                  icon: Icons.error_outline,
+                  iconSize: 32,
+                  iconColor: Colors.red,
+                  titleFontFamily: 'Raleway',
+                  titleFontSize: 22,
+                  titleFontColor: Colors.black54,
+                  descriptionFontFamily: 'Raleway',
+                  descriptionFontColor: Colors.black45,
+                  descriptionFontSize: 18,
+                  timerInSeconds: 2,
+                );
+              },
+              child: const Text('Show Error Message'),
+            ),
+            MaterialButton(
+              onPressed: () {
+                HAlertDialog.showCustomAlertBox(
+                  context: context,
+                  backgroundColor: Colors.blue,
+                  title: 'Info',
+                  description:
+                      'This is the message that will appear in case of Info',
+                  icon: Icons.info_outline,
+                  iconSize: 32,
+                  iconColor: Colors.blue,
+                  titleFontFamily: 'Raleway',
+                  titleFontSize: 22,
+                  titleFontColor: Colors.black54,
+                  descriptionFontFamily: 'Raleway',
+                  descriptionFontColor: Colors.black45,
+                  descriptionFontSize: 18,
+                  timerInSeconds: 2,
+                );
+              },
+              child: const Text('Show Info Message'),
+            ),
+            MaterialButton(
+              onPressed: () {
+                HAlertDialog.showCustomAlertBox(
+                  context: context,
+                  backgroundColor: Colors.yellow,
+                  title: 'Warning',
+                  description:
+                      'This is the message that will appear in case of Warning',
+                  icon: Icons.warning_amber_rounded,
+                  iconSize: 32,
+                  iconColor: Colors.yellow,
+                  titleFontFamily: 'Raleway',
+                  titleFontSize: 22,
+                  titleFontColor: Colors.black54,
+                  descriptionFontFamily: 'Raleway',
+                  descriptionFontColor: Colors.black45,
+                  descriptionFontSize: 18,
+                  timerInSeconds: 2,
+                );
+              },
+              child: const Text('Show Warning Message'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/h_alert_dialog.dart
+++ lib/h_alert_dialog.dart
@@ -5,12 +5,29 @@ import 'package:flutter/material.dart';
 class HAlertDialog {
   static Future showCustomAlertBox({
     required BuildContext context,
-    required Widget willDisplayWidget,
+    required int timerInSeconds,
+    required Color backgroundColor,
+    required String title,
+    required String description,
+    required IconData icon,
+    required int iconSize,
+    required Color iconColor,
+    required String titleFontFamily,
+    required double titleFontSize,
+    required Color titleFontColor,
+    required Color descriptionFontColor,
+    required String descriptionFontFamily,
+    required double descriptionFontSize,
   }) {
+    Future.delayed(Duration(seconds: timerInSeconds), () {
+      Navigator.pop(context);
+    });
+
     return showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
+            contentPadding: const EdgeInsets.all(0),
             shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(15)),
             ),
@@ -18,19 +35,56 @@ class HAlertDialog {
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 Container(
-                  height: 100,
                   width: double.infinity,
-                  color: Colors.blue,
-                  child: const Text('Image Here'),
+                  decoration: BoxDecoration(
+                    color: backgroundColor,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 24),
+                    child: SizedBox(
+                      height: 65,
+                      width: 65,
+                      child: CircleAvatar(
+                        radius: 65,
+                        backgroundColor: Colors.white,
+                        child: Icon(
+                          icon,
+                          color: iconColor,
+                          size: 32,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-                willDisplayWidget,
-                MaterialButton(
-                  color: Colors.white30,
-                  child: const Text('close alert'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                )
+                Padding(
+                  padding: const EdgeInsets.only(top: 22, bottom: 4),
+                  child: Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    maxLines: 1,
+                    style: TextStyle(
+                        color: titleFontColor,
+                        fontFamily: titleFontFamily,
+                        fontSize: titleFontSize,
+                        fontWeight: FontWeight.w700),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 14),
+                  child: Text(
+                    description,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: descriptionFontColor,
+                        fontFamily: descriptionFontFamily,
+                        fontSize: descriptionFontSize,
+                        fontWeight: FontWeight.w300),
+                  ),
+                ),
               ],
             ),
             elevation: 10,
